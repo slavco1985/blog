0f29772b2e52e2bea79391520d4408a003df6a2f
Slavco Nikolovski <slavco1985@gmail.com>
2018-04-15 00:11:28 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,68 @@
-<?php
-include './header.php';
-?>
-<div class="container">
 
 <?php
-include'./posts.php';
-include './right.php';
-include './footer.php';
+include 'db.php';
+
+
+
+$page = 1;
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+$limit = 5;
+$offset = ($page * $limit) - $limit;
+
+include 'header.php';
+include 'carousel.php';
+include 'right.php';
+
+include 'posts.php';
+
+$sql_page = "SELECT * FROM posts";
+$result_page = mysqli_query($con, $sql_page);
+$count = mysqli_num_rows($result_page);
+
+$a = $count / 5;
+$a = ceil($a);
+$prev = $page - 1;
+$next = $page + 1;
 ?>
+<div class="container ">
+    <div class="row">
 
+        <ul class="pagination ">
 
+            <?php
+            if ($page > 1) {
 
+                echo "<li class=\"previous\"><a href=\"index.php?page=$prev \">Previous</a></li>";
+            }
 
+            for ($b = 1; $b <= $a; $b++) {
+                ?>
+                <li><a href="index.php?page=<?php echo $b; ?>"><?php echo $b; ?></a></li>
 
+    <?php
+}
+?>
+            <li class="next"><a href="index.php?page=<?php echo $next; ?>">Next</a></li>
+        </ul>
+
+    </div>
 </div>
 
-</body>
-</html>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 well">
+                <h5 accesskey=""class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<h5>
+
+                        </div>
+
+
+                        </div>
+                        </div>
+                        </footer> 
+
+                        </body>
+                        </html>
